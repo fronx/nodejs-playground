90322ab0023f845a40b3079c3506efb527f97062
Fronx <fronx@soundcloud.com>
2013-11-30 22:48:51 +0100

    level-promise makes it even prettier

--- erdapfel/test/external/test_level_db.js
+++ erdapfel/test/external/test_level_db.js
@@ -2,17 +2,17 @@ var assert = require('assert');
 var levelup = require('levelup');
 var test = require('../lib/test.js');
 var Promise = require('promise');
+var level_promise = require('level-promise');
 
 test.with_temp_dir(function (tmp_dir) {
   var db = levelup(tmp_dir, { valueEncoding: 'json' });
-  var db_put = Promise.denodeify(db.put.bind(db));
-  var db_get = Promise.denodeify(db.get.bind(db));
+  level_promise(db);
 
   [
     function test_put_get () {
       var that =
-        db_put('key', { a: 1, b: 2 }).then(function () {
-          return db_get('key');
+        db.put('key', { a: 1, b: 2 }).then(function () {
+          return db.get('key');
         });
 
       function test_value (value) {
