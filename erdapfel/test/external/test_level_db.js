0a74483c6c78f7249eec69930c53ccc98acec81e
Fronx <fronx@soundcloud.com>
2013-11-30 19:22:37 +0100

    a test suite is an array. there are progress dots.

--- erdapfel/test/external/test_level_db.js
+++ erdapfel/test/external/test_level_db.js
@@ -4,19 +4,18 @@ var levelup = require('levelup');
 var db = levelup(TEST_DB_PATH, { valueEncoding: 'json' });
 var test = require('../lib/test.js');
 
-function test_put_get () {
-  db.put('key', { a: 1, b: 2 });
-  db.get('key', test.callbacks([
-    function (err, value) {
-      assert.equal(null, err);
-    },
-    function (err, value) {
-      assert.equal(
-        JSON.stringify({ a: 1, b: 2 })
-      , JSON.stringify(value)
-      );
-    }
-  ]));
-}
-
-test_put_get();
+[ function test_put_get () {
+    db.put('key', { a: 1, b: 2 });
+    db.get('key', test.callbacks([
+      function (err, value) {
+        assert.equal(null, err);
+      },
+      function (err, value) {
+        assert.equal(
+          JSON.stringify({ a: 1, b: 2 })
+        , JSON.stringify(value)
+        );
+      }
+    ]));
+  }
+].forEach(test.run);
--- erdapfel/test/lib/test.js
+++ erdapfel/test/lib/test.js
@@ -3,8 +3,19 @@ var callbacks = function (test_functions) {
     var args = arguments;
     test_functions.forEach(function (test_function) {
       test_function.apply(this, args);
+      progress();
     });
   }
 }
 
+var progress = function () {
+  process.stdout.write('.');
+}
+
+var run = function (fn)  {
+  return fn.call();
+}
+
 module.exports.callbacks = callbacks;
+module.exports.progress  = progress;
+module.exports.run       = run;
